76ba6f82cce99bc8ef6fdacd722513bf5458b515
Ronei Herter <roneiherter@gmail.com>
2024-11-05 21:52:32 -0300

    classe Funcções + main criados

---
+++ src/objetos/Funcoes.java
@@ -0,0 +1,11 @@
+package objetos;
+
+import java.util.Scanner;
+
+public class Funcoes {
+	
+	public static Scanner scan = new Scanner(System.in);
+	
+	// Aqui voce cria as funções
+
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,15 @@
+package principal;
+
+import objetos.Funcoes;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Funcoes funcao = new Funcoes();
+		
+		// aqui voce puxa a funções (Esse é o sistema principal)
+
+	}
+
+}
